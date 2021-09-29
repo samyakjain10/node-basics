fe180411bee705670a6978faaaa60836cffe0eca
Samyak Jain <sjsamyak2001@gmail.com>
2021-09-29 11:36:39 +0530

    Installed express.js

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const http = require("http");
+const express = require("express");
 
-const routes = require("./routes");
+const app = express();
 
-const server = http.createServer(routes);
+const server = http.createServer(app);
 
 server.listen(3000);
